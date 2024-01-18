bf7f82feb592c4e0ae1be8d37b4527d2ee8e1d4e
katerynaavetisyan <katerynaavetisyan@gmail.com>
2024-01-18 12:09:05 -0600

    colors added

--- src/ColorList.jsx
+++ src/ColorList.jsx
@@ -1,9 +1,10 @@
 export default function ColorList({ colors }) {
-    const lis = colors.map((color) => <li>{color}</li>)
+
     return (
         < div >
             <p>Color List</p>
-            <ul>{lis}</ul>
+            <ul>
+                {colors.map((c) => (<li style={{ color: c }}>{c}</li>))}</ul>
         </div >
     )
 }
