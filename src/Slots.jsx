689047d185ffe7292028393e8eb6eeeab96f0910
katerynaavetisyan <katerynaavetisyan@gmail.com>
2024-01-19 12:12:15 -0600

    slots machine exercise

---
+++ src/Slots.jsx
@@ -0,0 +1,9 @@
+export default function Slots({ val1, val2, val3 }) {
+    return (
+        <div>
+            <h1>
+                {val1} {val2} {val3}
+            </h1>
+        </div>
+    )
+}
