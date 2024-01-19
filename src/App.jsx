e200655b620eae227af664524124b281ae818865
katerynaavetisyan <katerynaavetisyan@gmail.com>
2024-01-19 08:52:59 -0600

    slots added

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,13 @@
-import Chicken from "./Chicken";
+
 import './App.css'
-import Greeter from "./Greeter";
-import Die from "./Die";
-import ListPicker from "./ListPicker";
-import DoubleDice from "./DoubleDice";
-import Heading from "./Heading";
-import ColorList from "./ColorList";
+import Slots from "./Slots";
+
 
 function App() {
   return (
     <div>
-      <ColorList colors={["red", "green", "yellow", "violet"]} />
-
-
-      <Heading color="Magenta" text="Welcome" fontSize="20px" />
-      <DoubleDice />
-      <DoubleDice />
-      <Heading color="Orange" text="Bye" fontSize="30px" />
-      {/* <DoubleDice />
-      <ListPicker values={[1, 2, 3]} />
-      <ListPicker values={["a", "b", "c"]} />
-      <ListPicker values={{ a: 1, b: 2 }} />
-      <Greeter person="Artur" />
-      <Greeter from="Colt" />
-      <Greeter person="David" from="Neighbor" />
-      <Die numSides={20} />
-      <Die />
-      <Die numSides={10} /> */}
+      <Slots val1="🍒" val2="🍒" val3="🍒" />
+      <Slots val1="🍒" val2="🍌" val3="🍒" />
     </div>
   )
 }
