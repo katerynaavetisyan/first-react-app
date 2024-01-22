9edbe13ac39d9b07c9d58299efb36b0f5e3513f9
katerynaavetisyan <katerynaavetisyan@gmail.com>
2024-01-22 16:24:41 -0600

    slots game finished

--- src/Slots.jsx
+++ src/Slots.jsx
@@ -1,9 +1,11 @@
 export default function Slots({ val1, val2, val3 }) {
+    const isWinner = val1 === val2 && val1 === val3;
     return (
         <div>
             <h1>
                 {val1} {val2} {val3}
             </h1>
+            <h2>{isWinner ? "You win!" : "You lose"}</h2>
         </div>
     )
 }
