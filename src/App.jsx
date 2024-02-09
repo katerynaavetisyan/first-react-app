739391722a670890892d31adc33abc91f1c698c3
katerynaavetisyan <katerynaavetisyan@gmail.com>
2024-02-09 11:34:04 -0600

    Property exersice updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import PropertyList from './PropertyList';
 
 const properties = [
   { id: 129031, name: 'Desert Yurt', rating: 4.9, price: 150 },
-  { id: 129032, name: 'Desert Mountain', rating: 4.8, price: 250 },
+  { id: 129032, name: 'Mountain', rating: 4.8, price: 250 },
   { id: 129033, name: 'Cactus Retreat', rating: 4.5, price: 350 },
   { id: 129034, name: 'Oceanview Condo', rating: 4.7, price: 450 }
 ];
--- src/PropertyList.jsx
+++ src/PropertyList.jsx
@@ -5,8 +5,10 @@ export default function PropertyList({ properties }) {
     return (
         <div className="PropertyList">
             {properties.map((p) => {
-                return <Property name={p.name} {...p} key={p.id} />;
+                return <Property  {...p} key={p.id} />;
             })}
         </div>
+
+
     )
 }
