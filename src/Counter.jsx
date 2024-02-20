1f27fe311f77fb03e1e9dc34bf409a45671cfa63
katerynaavetisyan <katerynaavetisyan@gmail.com>
2024-02-20 15:42:00 -0600

    working with the useState

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,6 +1,11 @@
 import { useState } from "react";
+
 export default function Counter() {
     const [num, setNum] = useState(5);
+
+    const changeNum = () => {
+        setNum(10)
+    }
 
 
     return (
